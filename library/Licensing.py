dc35f210b190552365eb2b5f3d654e4090698166
MasonicTombomb <136269520+MasonicTombomb@users.noreply.github.com>
2023-10-01 17:52:21 +0200

    This is the addition of the generation of a Unique Identifer for each system.
    This does have a known limitation of if any new network devices are added the unique identifier will be changed.

--- library/Licensing.py
+++ library/Licensing.py
@@ -1,8 +1,11 @@
 import requests
 from datetime import datetime
+import hashlib
+import platform
+import psutil
 
 
-def get_licence(licence_key):
+def getLicence(licence_key):
     server_url = 'http://10.20.10.112/licences'
     url = f'{server_url}/{licence_key}'
     print(url)
@@ -25,6 +28,34 @@ def get_licence(licence_key):
         print("Failed to connect to the licensing server.")
 
 
+def getUniqueID():
+    # Get MAC addresses of all network interfaces
+    mac_addresses = []
+    for interface, addresses in psutil.net_if_addrs().items():
+        for address in addresses:
+            if address.family == psutil.AF_LINK:
+                mac_addresses.append(address.address)
+
+    # Get system information
+    system_info = platform.system()
+    node = platform.node()
+    release = platform.release()
+    version = platform.version()
+
+    # Combine MAC addresses and system information into a unique string
+    unique_string = ''.join(mac_addresses) + system_info + node + release + version
+
+    # Generate a unique hash using SHA-256
+    unique_hash = hashlib.sha256(unique_string.encode()).hexdigest()
+
+    # Combine with a random UUID to add more uniqueness
+    unique_id = f"{unique_hash}"
+
+    return unique_id
+
+
+
+
 if __name__ == '__main__':
     licence_key = input("Enter your licence key: ")
-    get_licence(licence_key)
+    getLicence(licence_key)
